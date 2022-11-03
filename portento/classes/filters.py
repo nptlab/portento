8675e7aa861ac57c7860e82574598ca0b096a71a
RikJux <60518260+RikJux@users.noreply.github.com>
2022-11-03 10:29:55 +0100

    filter!

--- portento/classes/filters.py
+++ portento/classes/filters.py
@@ -39,7 +39,7 @@ class NoFilter(Filter):
 
 class TimeFilter(Filter):
     def __init__(self, list_of_intervals: List[Interval]):
-        self._interval_tree = StreamTree(list_of_intervals)
+        self._interval_tree = IntervalTree(list_of_intervals)
 
     @property
     def interval_tree(self):
@@ -76,27 +76,6 @@ class TimeFilter(Filter):
         return False
 
 
-def filter_by_time(tree_view: StreamTree, time_filter: Union[NoFilter, TimeFilter]):
-    if tree_view.root:
-        return iter(_filter_node_by_time(tree_view.root, time_filter))
-    else:
-        return iter()
-
-
-def _filter_node_by_time(node: StreamTreeNode, time_filter):
-    if time_filter(node.full_interval):
-        if node.left:
-            yield from _filter_node_by_time(node.left, time_filter)
-        if time_filter(node.value):
-            for interval in time_filter[node.value]:
-                if hasattr(node, 'u'):
-                    yield Link(interval, node.u, node.v)
-                else:
-                    yield interval
-        if node.right:
-            yield from _filter_node_by_time(node.right, time_filter)
-
-
 class NodeFilter(Filter):
     def __init__(self, filter_nodes: Callable):
         self._filter = filter_nodes
@@ -110,6 +89,27 @@ class NodeFilter(Filter):
     def __call__(self, *args, **kwargs):
         node = args[0]
         return self._filter(node)
+
+
+def filter_by_time(tree_view: StreamTree, time_filter: Union[NoFilter, TimeFilter]):
+    if tree_view.root:
+        return iter(_filter_node_by_time(tree_view.root, time_filter))
+    else:
+        return iter()
+
+
+def _filter_node_by_time(node: Union[StreamTreeNode, IntervalTreeNode], time_filter):
+    if time_filter(node.full_interval):
+        if node.left:
+            yield from _filter_node_by_time(node.left, time_filter)
+        if time_filter(node.value):
+            for interval in time_filter[node.value]:
+                if hasattr(node, 'u'):
+                    yield Link(interval, node.u, node.v)
+                else:
+                    yield interval
+        if node.right:
+            yield from _filter_node_by_time(node.right, time_filter)
 
 
 def filter_by_nodes(dict_view: StreamDict, node_filter: Union[NoFilter, NodeFilter]):
@@ -128,27 +128,9 @@ def filter_stream(stream: Stream,
 
     elif isinstance(time_filter, NoFilter) or first == 'node':  # node first slice
         yield from merge(*(map(lambda x: Link(*x),
-                               zip(filter_by_time(links._intervals, time_filter), repeat(u), repeat(v)))
+                               zip(filter_by_time(links.interval_tree, time_filter), repeat(u), repeat(v)))
                            for u, adj in stream.edges.items() if node_filter(u)
                            for v, links in adj.items() if node_filter(v)
                            ))
     else:
         raise Exception
-
-
-new_links = [Link(Interval(1.0, 3.0, 'both'), 'a', 'b'),
-             Link(Interval(7.0, 8.0, 'both'), 'a', 'b'),
-             Link(Interval(4.5, 7.5, 'both'), 'a', 'c'),
-             Link(Interval(6.0, 9.0, 'both'), 'b', 'c'),
-             Link(Interval(2.0, 3.0, 'both'), 'b', 'd'),
-             Link(Interval(0.0, 4.0, 'both'), 'a', 'b'),
-             Link(Interval(6.0, 9.0, 'both'), 'a', 'b'),
-             Link(Interval(2.0, 5.0, 'both'), 'a', 'c'),
-             Link(Interval(1.0, 8.0, 'both'), 'b', 'c'),
-             Link(Interval(7.0, 10.0, 'both'), 'b', 'd'),
-             Link(Interval(6.0, 9.0, 'both'), 'c', 'd')]
-
-s = Stream(new_links)
-print(
-    [i for i in
-     filter_stream(s, NodeFilter(lambda x: x in ['a', 'b', 'c']), TimeFilter([Interval(4.0, 6.0, 'both')]), 'node')])

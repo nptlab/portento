839323b62daff805716e12ad36ff73ccbe8bb8a6
RikJux <60518260+RikJux@users.noreply.github.com>
2023-02-09 12:45:33 +0100

    importing ok

--- portento/classes/__init__.py
+++ portento/classes/__init__.py
@@ -1,6 +1,6 @@
 from .streamdict import StreamDict, DiStreamDict
 from .streamtree import StreamTree, DiStreamTree
 from .stream import Stream, DiStream
-from .slice import slice_stream, slice_by_time, slice_by_nodes, \
+from .slice import slice_stream, slice_di_stream, slice_by_time, slice_by_nodes, \
     NoFilter, TimeFilter, NodeFilter
 from .metrics import *
